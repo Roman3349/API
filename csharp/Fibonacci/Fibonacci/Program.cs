68f96d7ad0011328618b77f94af5c657a7939a6d
Roman3349 <ondracek.roman@centrum.cz>
2016-09-13 09:25:44 +0200

    Přidán program v C# pro výpočet Fibonacciho čísla
    Signed-off-by: Roman3349 <ondracek.roman@centrum.cz>

---
+++ csharp/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fibonacci
+{
+	class MainClass
+	{
+		/**
+		 * Vypočítá Fibonacciho číslo
+		 * @param cislo Číslo, ze kterého chceme vypočítat Fibonacciho číslo
+		 * @return Fibonacciho číslo
+		 */
+		public static int Fibonacci(int cislo) {
+			if (cislo < 2) {
+				return(cislo);
+			} else {
+				return (Fibonacci(cislo - 1) + Fibonacci(cislo - 2));
+			}
+		}
+
+		public static void Main (string[] args)
+		{
+			Console.Write("Zadejte číslo, ze kterého chcete vypočítat Fibonacciho číslo: ");
+			int cislo = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine(Convert.ToString(Fibonacci(cislo)));
+		}
+	}
+}
