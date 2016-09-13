aebcbf029e88c734d8877747c4182a4a2afec9bd
Roman3349 <ondracek.roman@centrum.cz>
2016-09-13 09:19:18 +0200

    Přidán program na výpočet faktoriálu v C#
    Signed-off-by: Roman3349 <ondracek.roman@centrum.cz>

---
+++ c#/Faktorial/Faktorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Faktorial
+{
+	class MainClass
+	{
+		/**
+		 * Vypočítá faktoriál
+		 * @param cislo Číslo, ze kterého chceme vypočítat faktoriál
+		 * @return Faktoriál
+		 */
+		public static int Faktorial(int cislo) {
+			if (cislo <= 0) {
+				return(1);
+			} else {
+				return (cislo * Faktorial(cislo - 1));
+			}
+		}
+
+		public static void Main (string[] args)
+		{
+			Console.Write("Zadejte číslo, ze kterého chcete vypočítat Faktoriál: ");
+			int cislo = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine(Convert.ToString(Faktorial(cislo)));
+		}
+	}
+}
