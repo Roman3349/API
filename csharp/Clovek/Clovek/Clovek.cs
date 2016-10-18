dba6ae8751c16900ac26754818e3a9cb13783803
Roman3349 <ondracek.roman@centrum.cz>
2016-10-18 09:19:36 +0200

    Add example with properties in C#
    Signed-off-by: Roman3349 <ondracek.roman@centrum.cz>

---
+++ csharp/Clovek/Clovek/Clovek.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Clovek
+{
+	public class Clovek
+	{
+
+		public string jmeno;
+		public string prijmeni;
+		public int vek;
+		public string pozdrav;
+
+		public Clovek() {
+
+		}
+
+		public Clovek (string jmeno, string prijmeni, int vek, string pozdrav)
+		{
+			this.jmeno = jmeno;
+			this.prijmeni = prijmeni;
+			this.vek = vek;
+			this.pozdrav = pozdrav;
+		}
+	}
+}
+
---
+++ csharp/Clovek/Clovek/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+namespace Clovek
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			ArrayList lide = new ArrayList ();
+			//lide.Add (new Clovek("Petr", "Drahos", 38, "Dobrý den!"));
+			//lide.Add (new Clovek("Petr", "Mikulášek", 37, "Dobrý den!"));
+			Console.WriteLine ("Zadej 4 lidi: ");
+			for (int i = 0; i < 4; i++) {
+				Console.WriteLine ("**********************************************");
+				Console.Write ("Jméno: ");
+				string jmeno = Console.ReadLine ();
+				Console.Write ("Příjmení: ");
+				string prijmeni = Console.ReadLine ();
+				Console.Write ("Věk: ");
+				int vek = Convert.ToInt16(Console.ReadLine ());
+				Console.Write ("Pozdrav: ");
+				string pozdrav = Console.ReadLine ();
+				lide.Add (new Clovek(jmeno, prijmeni, vek, pozdrav));
+			}
+			Console.WriteLine ("**********************************************");
+			bool nalezen = false;
+			Console.Write ("Jméno hledané osoby: ");
+			string _jmeno = Console.ReadLine ();
+			if (!nalezen) {
+				foreach (Clovek clovek in lide) {
+					if (clovek.jmeno == _jmeno) {
+						Console.WriteLine ("**********************************************");
+						Console.WriteLine ("Jméno: " + clovek.jmeno + " " + clovek.prijmeni);
+						Console.WriteLine ("Věk: " + Convert.ToString (clovek.vek));
+						nalezen = true;
+					}
+				}
+				if (!nalezen) {
+					Console.WriteLine ("Neznám člověka s tímto jménem.");
+				}
+			}
+		}
+	}
+}
