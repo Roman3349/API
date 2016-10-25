a11e0a7f51aea07ef9958e98cfc3cd72ec64455c
Roman3349 <ondracek.roman@centrum.cz>
2016-10-25 09:33:18 +0200

    Úprava úkázky s výjimkami
    Signed-off-by: Roman3349 <ondracek.roman@centrum.cz>

--- csharp/Vyjimky/Vyjimky/Program.cs
+++ csharp/Vyjimky/Vyjimky/Program.cs
@@ -7,26 +7,34 @@ namespace Vyjimky
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Dělení 2 čísel:");
-			int x = 0;
-			int y = 0;
-			float result = 0;
+			double x = 0.0;
+			double y = 0.0;
+			double result = 0;
 
 			try {
 				Console.Write("Zadej 1. číslo: ");
-				x = Convert.ToInt32(Console.ReadLine());
+				x = OsetriCislo(Console.ReadLine());
 				Console.Write("Zadej 2. číslo: ");
-				y = Convert.ToInt32(Console.ReadLine());
+				y = OsetriCislo(Console.ReadLine());
 			} catch (FormatException e) {
 				Console.WriteLine("Neumíš napsat číslo?");
 			}
 
 			try {
-				result = x / y;
+				result = Deleni(x, y);
 			} catch (DivideByZeroException e) {
 				Console.WriteLine("Dělit nulou nelze!");
 			} finally {
 				Console.WriteLine("Výsledek dělení je " + Convert.ToString(result));
 			}
+		}
+
+		public static double OsetriCislo(String x) {
+			return (Convert.ToDouble(x));
+		}
+
+		public static double Deleni (double x, double y) {
+			return (x / y);
 		}
 	}
 }
