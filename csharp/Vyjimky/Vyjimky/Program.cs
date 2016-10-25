e317da9eeeb8fc78d31811a1832b4751333e02ef
Roman3349 <ondracek.roman@centrum.cz>
2016-10-25 09:01:34 +0200

    Přidána ukázka výjimek v C#
    Signed-off-by: Roman3349 <ondracek.roman@centrum.cz>

---
+++ csharp/Vyjimky/Vyjimky/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Vyjimky
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Dělení 2 čísel:");
+			int x = 0;
+			int y = 0;
+			float result = 0;
+
+			try {
+				Console.Write("Zadej 1. číslo: ");
+				x = Convert.ToInt32(Console.ReadLine());
+				Console.Write("Zadej 2. číslo: ");
+				y = Convert.ToInt32(Console.ReadLine());
+			} catch (FormatException e) {
+				Console.WriteLine("Neumíš napsat číslo?");
+			}
+
+			try {
+				result = x / y;
+			} catch (DivideByZeroException e) {
+				Console.WriteLine("Dělit nulou nelze!");
+			} finally {
+				Console.WriteLine("Výsledek dělení je " + Convert.ToString(result));
+			}
+		}
+	}
+}
