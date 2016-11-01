245817cc3c1f470292312bb6043f87ec1b14e5c3
Roman3349 <ondracek.roman@centrum.cz>
2016-11-01 08:41:41 +0100

    Přidána ukázka práce se znaky
    Signed-off-by: Roman3349 <ondracek.roman@centrum.cz>

---
+++ csharp/Znaky/Znaky/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Znaky
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string text = "Programátor se zasekne ve sprše, protože instrukce na šampónu byly: Namydlit, omýt, opakovat.";
+			string samohlasky = "aeiouyáéěíóúůý";
+			string souhlasky = "bcčdďfghjklmnpqrřsštťvwxzž";
+			int pocetSamohlasek = 0;
+			int pocetSouhlasek = 0;
+			int pocetOstatnich = 0;
+			Console.WriteLine(text);
+			text.ToLower();
+
+			foreach (char znak in text) {
+				if (samohlasky.Contains (Convert.ToString(znak))) {
+					pocetSamohlasek++;
+				} else if (souhlasky.Contains (Convert.ToString(znak))) {
+					pocetSouhlasek++;
+				} else {
+					pocetOstatnich++;
+				}
+			}
+
+			Console.WriteLine ("================ Analýza textu ================");
+			Console.WriteLine ("Počet samohlásek: " + Convert.ToString(pocetSamohlasek));
+			Console.WriteLine ("Počet souhlásek: " + Convert.ToString(pocetSouhlasek));
+			Console.WriteLine ("Počet ostatních znaků: " + Convert.ToString(pocetOstatnich));
+		}
+	}
+}
