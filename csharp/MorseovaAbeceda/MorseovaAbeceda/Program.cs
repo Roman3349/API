bb0217cf00df9b25c9a0677ef2773d2d407fdab8
Roman3349 <ondracek.roman@centrum.cz>
2016-11-08 09:20:46 +0100

    Přidáno dešifrování Morsevy abecedy
    Signed-off-by: Roman3349 <ondracek.roman@centrum.cz>

--- csharp/MorseovaAbeceda/MorseovaAbeceda/Program.cs
+++ csharp/MorseovaAbeceda/MorseovaAbeceda/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MorseovaAbeceda
@@ -31,12 +33,10 @@ namespace MorseovaAbeceda
 			StringBuilder zasifrovanyText = new StringBuilder ();
 			string mod;
 			Console.Write ("Chcete převést text do Morseovy abecedy? [A/n] ");
-			mod = Console.ReadLine ();
-			mod = mod.ToLower ();
+			mod = Console.ReadLine ().ToLower ();
 			if (mod.Equals ("a")) {
 				Console.Write ("Napište text, který chcete převést do Morseovy abecedy: ");
-				text = Console.ReadLine ();
-				text = text.ToLower ();
+				text = Console.ReadLine ().ToLower ();
 				foreach (char znak in text) {
 					if (abeceda.ContainsKey (znak)) {
 						zasifrovanyText.Append (abeceda [znak]);
@@ -45,7 +45,21 @@ namespace MorseovaAbeceda
 				}
 				Console.WriteLine ("Text v Morseově abecedě: " + zasifrovanyText.ToString ());
 			} else if (mod.Equals ("n")) {
-				
+				Console.WriteLine ("Znaky Morseovy abecedy oddělujte merezami.");
+				Console.Write ("Napište text, který chcete převést do abecedy: ");
+				text = Console.ReadLine ().ToLower ();
+				string[] pole = new string[text.Length];
+				pole = text.Split (' ');
+				foreach (string znak in pole) {
+					if (abeceda.ContainsValue (znak)) {
+						foreach (KeyValuePair<char, string> klicHodnota in abeceda) {
+							if (klicHodnota.Value == znak) {
+								zasifrovanyText.Append (klicHodnota.Key);
+							}
+						}
+					}
+				}
+				Console.WriteLine ("Dešifrovaný text: " + zasifrovanyText.ToString ());
 			}
 		}
 	}
