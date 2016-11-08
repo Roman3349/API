30933e7acc42cf2f50bbce350ec61d4f0678d1f5
Roman3349 <ondracek.roman@centrum.cz>
2016-11-08 08:13:10 +0100

    Přidána ukázka se slovníkem
    Signed-off-by: Roman3349 <ondracek.roman@centrum.cz>

---
+++ csharp/Slovnik/Slovnik/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Slovnik
+{
+	class MainClass
+	{
+		private static Dictionary<string, string> words = new Dictionary<string, string> () {
+			{"hello", "ahoj"}, {"goodbye", "nashledanou"}, {"good morning", "dobré ráno"}, 
+			{"boss", "šéf"}, {"", ""}, {"", ""}, {"", ""},
+			{"key", "klíč"}, {"value", "hodnota"}, {"", ""}, {"", ""},
+			{"computer", "počítač"}, {"keyboard", "klávesnice"}, {"", ""}, {"", ""},
+			{"glasses", "brýle"}, {"", ""}, {"", ""}, {"", ""},
+			{"head ache", "bolest hlavy"}, {"toothache", "bolest zubu"},
+			{"", ""}, {"", ""}, {"", ""}, {"", ""},
+			{"", ""}, {"", ""}, {"", ""}, {"", ""},
+			{"", ""}, {"", ""}, {"", ""}, {"", ""},
+			{"", ""}, {"", ""}, {"", ""}, {"", ""},
+			{"", ""}, {"", ""}, {"", ""}, {"", ""},
+			{"", ""}, {"", ""}, {"", ""}, {"", ""},
+			{"", ""}, {"", ""}, {"", ""}, {"", ""}
+			
+		}
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
