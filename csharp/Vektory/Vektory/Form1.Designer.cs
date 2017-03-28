eab8ec2fc76302dbce17ef6119a08942f7c392a7
Roman3349 <ondracek.roman@centrum.cz>
2017-03-28 09:25:40 +0200

    Add vectors
    Signed-off-by: Roman3349 <ondracek.roman@centrum.cz>

---
+++ csharp/Vektory/Vektory/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Vektory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.u1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.u2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.v2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.v1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vektor u (";
+            // 
+            // u1
+            // 
+            this.u1.Location = new System.Drawing.Point(75, 12);
+            this.u1.Name = "u1";
+            this.u1.Size = new System.Drawing.Size(40, 20);
+            this.u1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "; ";
+            // 
+            // u2
+            // 
+            this.u2.Location = new System.Drawing.Point(140, 12);
+            this.u2.Name = "u2";
+            this.u2.Size = new System.Drawing.Size(40, 20);
+            this.u2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(186, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = ")";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(379, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = ")";
+            // 
+            // v2
+            // 
+            this.v2.Location = new System.Drawing.Point(333, 12);
+            this.v2.Name = "v2";
+            this.v2.Size = new System.Drawing.Size(40, 20);
+            this.v2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(314, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "; ";
+            // 
+            // v1
+            // 
+            this.v1.Location = new System.Drawing.Point(268, 12);
+            this.v1.Name = "v1";
+            this.v1.Size = new System.Drawing.Size(40, 20);
+            this.v1.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(209, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Vektor v (";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(8, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(480, 480);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(409, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Vykresli";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 537);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.v2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.v1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.u2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.u1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Vykreslení vektorů";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox u1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox u2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox v2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox v1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ csharp/Vektory/Vektory/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Vektory
+{
+    public partial class Form1 : Form
+    {
+        Graphics gfx;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.gfx = this.pictureBox1.CreateGraphics();
+        }
+
+        private void vykresliOsy(Graphics gfx)
+        {
+            gfx.DrawLine(Pens.Black, 0, 240, 480, 240);
+            gfx.DrawLine(Pens.Black, 240, 0, 240, 480);
+        }
+
+        private void vykresliVektory(Graphics gfx, int v1, int v2, int u1, int u2)
+        {
+            int s = 240;
+            int _u1 = s + u1;
+            int _u2 = s - u2;
+            int _v1 = s + v1;
+            int _v2 = s - v2;
+            gfx.Clear(this.pictureBox1.BackColor);
+            gfx.DrawLine(Pens.Blue, s, s, _v1, _v2);
+            gfx.DrawLine(Pens.Green, s, s, _u1, _u2);
+            //gfx.DrawLine(Pens.Green, _v1, _v2, _v1 + u1, _v2 - u2);
+            //gfx.DrawLine(Pens.Green, _u1, _u2, _u1 + v1, _u2 - v2);
+            gfx.DrawLine(Pens.Red, s, s, _u1 + v1, _u2 - v2);
+            gfx.DrawLine(Pens.Violet, _v1, _v2, _u1, _u2);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.vykresliOsy(this.gfx);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int u1 = Convert.ToInt32(this.u1.Text);
+            int u2 = Convert.ToInt32(this.u2.Text);
+            int v1 = Convert.ToInt32(this.v1.Text);
+            int v2 = Convert.ToInt32(this.v2.Text);
+            this.vykresliVektory(this.gfx, v1, v2, u1, u2);
+            this.vykresliOsy(this.gfx);
+        }
+    }
+}
